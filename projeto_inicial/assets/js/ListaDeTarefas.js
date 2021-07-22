d18ae07c47e99bd54ebdaff522ee80adefbf9cf7
Jean Everton <jeanever39@gmail.com>
2021-07-22 09:04:11 -0300

    pegando informação do input

--- projeto_inicial/assets/js/ListaDeTarefas.js
+++ projeto_inicial/assets/js/ListaDeTarefas.js
@@ -1,5 +1,10 @@
 const novaTarefa = document.querySelector("[data-form-button]");
-// criando um evento de click 
-novaTarefa.addEventListener("click", () => {
-  console.log("fui clicado");
+
+
+
+// criando um evento de click
+novaTarefa.addEventListener("click", () => { // ao clicar 
+  const input = document.querySelector("[data-form-input]"); // coleta a informação do input
+  const valor = input.value // pega a informação e retorna oque foi passado
+  console.log(valor); // mostra o que foi passado
 });
